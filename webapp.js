b665f83b5d026755e33c8c979f5388ab5ebd0791
mbhalsa <mbhalsmmm@gmail.com>
2026-04-14 09:46:40 +0300

    Remove unnecessary blank line in webapp.jsImplement application routes, session middleware, and secure file handling

--- webapp.js
+++ webapp.js
@@ -5,7 +5,6 @@ const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
 const business = require('./business.js')
-
 const app = express()
 
 app.set('view engine', 'hbs')
